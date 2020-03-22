6555fef64f2776149002ccdd03c5b2a790cce336
Edwin Rono <portaldeveloper@gmail.com>
2020-03-22 13:51:12 +0300

    Edwin- Added Signout

---
+++ DynamicsNAV365-Staff-WebPortal/DynamicsNAV365-Staff-WebPortal/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicsNAV365_Staff_WebPortal.Controllers
+{
+    public class AccountController : Controller
+    {
+		#region Signout
+		public ActionResult Logout()
+		{
+			return View();
+		}
+		#endregion Signout
+	}
+}
